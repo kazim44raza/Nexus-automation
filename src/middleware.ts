39cea206d493220d87e4dc190e6d7a3b57c76dfb
Kazim Raza <kazim44raza@gmail.com>
2026-08-04 00:15:42 +0500

    Serve edge-safe transparent mobile icon

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,21 +10,15 @@ import { authConfig } from '@/lib/auth.config'
  */
 const { auth } = NextAuth(authConfig)
 
-const TRANSPARENT_PNG = Uint8Array.from([
-  137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82,
-  0, 0, 0, 1, 0, 0, 0, 1, 8, 6, 0, 0, 0, 31, 21, 196,
-  137, 0, 0, 0, 13, 73, 68, 65, 84, 8, 215, 99, 96, 96, 96,
-  0, 0, 0, 5, 0, 1, 226, 38, 5, 155, 0, 0, 0, 0, 73, 69,
-  78, 68, 174, 66, 96, 130,
-])
+const TRANSPARENT_ICON = '<svg xmlns="http://www.w3.org/2000/svg" width="1" height="1" viewBox="0 0 1 1"></svg>'
 
 export default auth((req) => {
   const { pathname } = req.nextUrl
 
   if (pathname === '/favicon.ico') {
-    return new NextResponse(TRANSPARENT_PNG, {
+    return new NextResponse(TRANSPARENT_ICON, {
       headers: {
-        'Content-Type': 'image/png',
+        'Content-Type': 'image/svg+xml; charset=utf-8',
         'Cache-Control': 'no-store, no-cache, must-revalidate, max-age=0',
         Pragma: 'no-cache',
         Expires: '0',
